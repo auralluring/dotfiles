d841eb75053df9aa23f39da002fe5b88d63fb526
Aura <45722265+auralluring@users.noreply.github.com>
2024-09-10 11:03:20 -0600

    have auto automatically link itself to PATH
    also following that link back to the dotfiles dir, obviously, but then
    actually cd'ing into there so i don't have to remember to put
    '$DOTFILES_DIR/' in front every path to a dotfile throughout the
    scripts. may actually get some of the subscripts to cd into their own
    dirs as well.

--- auto.sh
+++ auto.sh
@@ -1,24 +1,37 @@
 #!/bin/env bash
 
-DOTFILES_DIR=${0%/*}
-
+# what options should be set on virtually every link made (with ln)
+# helps to make it consistent and fix bugs with broken links across all the scripts.
 LINKOPTS=-srf
 
+if [[ ${0%/*} == ~/.local/bin ]]; then
+   EXECUTABLE=$(readlink -f $0)
+else
+   EXECUTABLE=$0
+   ln $LINKOPTS $0 ~/.local/bin/auto
+fi
+
+DOTFILES_DIR=${EXECUTABLE%/*}
+cd $DOTFILES_DIR
+
 # gather data, put in env
-source $DOTFILES_DIR/hosts/$HOSTNAME.sh
+source hosts/$HOSTNAME.sh
+
+# get theme
+source themes/$THEME.sh
 
 # run core
-source $DOTFILES_DIR/core.sh
+source core.sh
 
 # run common
-source $DOTFILES_DIR/common/*.sh
+source common/*.sh
 
 # run rices based on data
 if [[ -z $RICES ]]; then
     echo No rices specified, no graphical environment will be installed.
 else
-    source $DOTFILES_DIR/rices/{$RICES,}.sh
+    source rices/{$RICES,}.sh
 fi
 
 # run apps based on data
-source $DOTFILES_DIR/apps/{$APPS,}.sh
+source apps/{$APPS,}.sh
